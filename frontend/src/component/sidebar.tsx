76960f33afbbbb131d95a7d6aa78c6c7c79b7434
kiran <kummkiran@gmail.com>
2026-07-29 18:53:58 +0530

    restructing my app

--- frontend/src/component/sidebar.tsx
+++ frontend/src/component/sidebar.tsx
@@ -1,13 +1,15 @@
-import Sidebaritem from './sidebaritem'
+import Sidebaritem from './ui/sidebaritem'
 import Document from '../icons/doument';
-import Twitter from './ui/tweet';
-import Youtube from './ui/youtube';
+import Twitter from '../icons/tweet';
+import Youtube from '../icons/youtube';
+import { LuBrain } from "react-icons/lu";
+
 
 const sidebar = () => {
   return (
-    <section id="sidebar" className='w-1/4 p-0'>
-      <div className='p-4 fixed top-0 left-0 h-full w-1/4 bg-white border-r border-2 border-gray-300'>
-        <h2 className='text-lg font-semibold mb-4'>Second Brain</h2>
+    <section id="sidebar" className='w-[20%] p-0'>
+      <div className='p-4 fixed top-0 left-0 h-full w-[20%] text-gray-600 bg-white border-r border-2 border-gray-300'>
+        <h2 className='text-lg font-semibold mb-4  flex items-center gap-2'><LuBrain size={24} /> Second Brain</h2>
         <ul className='space-y-2'>
           <li>
             <Sidebaritem title="Tweets" icon={<Twitter />} onClick={() => console.log("Home clicked")} />
---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import Button from '../component/ui/button'
+import { Plus } from '../icons/plus'
+import {Share} from '../icons/share'
+import Card from '../component/ui/card'
+import Model from '../component/model'
+import Sidebar from '../component/sidebar'
+
+function Dashboard() {
+  const [count, setCount] = useState(0)
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen)
+  } 
+
+  return (
+    <>
+    
+    <div className='flex min-h-screen'>
+      <Sidebar/> 
+
+    <div className='bg-background w-4/5'>
+
+    <div className='flex gap-4 mx-2 my-4 items-start'>
+      
+    {isOpen && (
+      <Model closeModal={toggleModal} />
+    )}
+      <Button variant="primary" onClick={toggleModal} startIcon={<Plus/>}>
+        Add content
+      </Button>
+       <Button variant="secondary" onClick={() => setCount(count + 1)} startIcon={<Share/>}>
+        share barin
+      </Button>
+    </div>
+    <div className='flex gap-3 mx-2 my-4 items-start'>
+      <Card title="My Card" link="https://www.youtube.com/watch?v=87T8xE-_yeo" type="youtube" tags={["React", "TypeScript"]} />
+      <Card title="My Card" link="https://x.com/JobFound5/status/2069081308756894065" type="tweet" tags={["React", "TypeScript"]} />
+      </div>    
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Dashboard
