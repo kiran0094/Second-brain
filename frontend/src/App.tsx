8617405a572e0819b0025a9b6976f5a73c3d7723
kiran <kummkiran@gmail.com>
2026-07-30 19:29:12 +0530

    added sigup page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import Dashboard from "./pages/Dashboard"
 import Signin from "./pages/Signin"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
+          <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/" element={<Dashboard />} />
         </Routes>
